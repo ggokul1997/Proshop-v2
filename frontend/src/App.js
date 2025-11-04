859685db4834235a61b139156d4fb707d490932a
G Gokul <ggokul1997@gmail.com>
2025-11-04 14:45:19 +0530

    router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Outlet } from "react-router-dom"
 
 const App = () => {
   return (
-    <div>Welcome to proshop</div>
+    <>
+    <Header/>
+      <main className="py-3">
+      <Container>
+        <Outlet/>
+      </Container>
+    
+    </main>
+    <Footer/>
+    </>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './assets/styles/index.css';
 import App from './App';
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/bootstrap.custom.css';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='/' index element={<HomeScreen/>}/>
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 

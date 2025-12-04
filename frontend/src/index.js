aa90723e648fa71cc769f0a8be914762930478c7
G Gokul <ggokul1997@gmail.com>
2025-12-05 01:48:29 +0530

    remove secret

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,8 @@ import reportWebVitals from './reportWebVitals';
 import './assets/styles/bootstrap.custom.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,7 +23,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>
 );
 
